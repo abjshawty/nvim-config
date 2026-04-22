c7da460e8bf8c7c4f8f7a359e0f55f5f3f65fe52
Timmy <kouadiobhegnino@gmail.com>
2026-04-22 17:35:20 +0000

    ++cyberdream theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,18 @@
 return {
   { "ellisonleao/gruvbox.nvim", lazy = true },
-  { "folke/tokyonight.nvim",    lazy = true },
-  { "catppuccin/nvim",          lazy = true },
-  { "tahayvr/matteblack.nvim",  lazy = true },
-  { "shaunsingh/nord.nvim",     lazy = true },
-  { "sainnhe/everforest",       lazy = true },
-  { "bjarneo/aether.nvim",      lazy = true },
-  { "bjarneo/ethereal.nvim",    lazy = true },
-  { "bjarneo/hackerman.nvim",   lazy = true },
+  { "folke/tokyonight.nvim", lazy = true },
+  { "catppuccin/nvim", lazy = true },
+  { "tahayvr/matteblack.nvim", lazy = true },
+  { "shaunsingh/nord.nvim", lazy = true },
+  { "sainnhe/everforest", lazy = true },
+  { "bjarneo/aether.nvim", lazy = true },
+  { "bjarneo/ethereal.nvim", lazy = true },
+  { "bjarneo/hackerman.nvim", lazy = true },
+  { "scottmckendry/cyberdream.nvim", lazy = true },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "matteblack",
+      colorscheme = "cyberdream",
     },
   },
 }
