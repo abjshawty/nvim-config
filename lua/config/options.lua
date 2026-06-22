33ef04133e6cf6b0dd0646be0bb8c31a4e48221d
Timmy <kouadiobhegnino@gmail.com>
2026-06-22 13:34:33 +0000

    fix: Default Shell
    Added a check. If OS is Windows, use pwsh, else use bash

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-LazyVim.terminal.setup("pwsh")
+local is_windows = package.config:sub(1, 1) == "\\"
+
+if is_windows then
+  LazyVim.terminal.setup("pwsh")
+  print("Running on Windows")
+else
+  LazyVim.terminal.setup("bash")
+  print("Running on Linux/macOS")
+end
